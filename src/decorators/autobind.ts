50210e30cb134238c9fbcc73ac95f83383330fcc
Mario Morales <mario.morales@devsu.com>
2022-05-24 13:59:27 -0500

    setup mutiple fils with Es6 modules

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,13 +1,11 @@
-namespace App {
-   export function AutoBind(_: any, _1: string, descriptor: PropertyDescriptor) {
-      const originalMethod = descriptor.value;
-      const adjDescriptor: PropertyDescriptor = {
-         configurable: true,
-         get() {
-            const boundFn = originalMethod.bind(this);
-            return boundFn;
-         }
+export function AutoBind(_: any, _1: string, descriptor: PropertyDescriptor) {
+   const originalMethod = descriptor.value;
+   const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+         const boundFn = originalMethod.bind(this);
+         return boundFn;
       }
-      return adjDescriptor;
    }
+   return adjDescriptor;
 }
