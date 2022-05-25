9a364686b7805d1f3d4e479ab453bb16dad8b643
Mario Morales <mario.morales@devsu.com>
2022-05-25 14:03:30 -0500

    added js packages

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
+import _ from 'lodash';
 import { ProjectInput } from './components/project-input';
 import { ProjectList } from './components/project-list';
+
+// declaring a global var or code that is available on a js script
+declare var GLOBAL: any;
 
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
+
+console.log(_.shuffle([1, 2, 3, 4, 5]));
+console.log(GLOBAL);
